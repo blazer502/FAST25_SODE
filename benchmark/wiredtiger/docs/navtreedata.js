379116435f837d7879a154b1854b1a0f51e22aae
chanyoung <blazer502@unist.ac.kr>
2024-12-12 15:50:17 +0900

    init

---
+++ benchmark/wiredtiger/docs/navtreedata.js
@@ -0,0 +1,32 @@
+var NAVTREE =
+[
+  [ "WiredTiger", "index.html", [
+    [ "Reference Guide", "index.html", "index" ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Class List", "annotated.html", "annotated" ],
+    [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+    [ "Class Members", "functions.html", [
+      [ "All", "functions.html", "functions_dup" ],
+      [ "Functions", "functions_func.html", null ],
+      [ "Variables", "functions_vars.html", null ],
+      [ "Enumerator", "functions_eval.html", null ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ],
+    [ "Community", "community.html", null ],
+    [ "License", "license.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"custom_data_sources.html#custom_ds_config",
+"group__wt.html#ga1c6ac48866cd180fb9089ce29db08d15",
+"group__wt.html#ga74576de052c86fb6bc8d465e3b2fb4fe",
+"group__wt.html#gacb3cbab83e6f6e940169629d549b2050",
+"schema.html#schema_examples",
+"threads_lang_java.html#threads_example_lang_java"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
